dcfc15561e6d514fedeed9252e38979210e7a276
Theron Boerner <hunterboerner@gmail.com>
2013-07-04 13:05:12 -0500

    fix shift click crash

--- QuantumCraft/inventory/ContainerQDeenergizer.java
+++ QuantumCraft/inventory/ContainerQDeenergizer.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerQDeenergizer extends Container {
 
@@ -30,6 +31,10 @@ public class ContainerQDeenergizer extends Container {
         }
 	}
 
+	public ItemStack transferStackInSlot(EntityPlayer player, int slot){
+		return null;
+	}
+	
 	@Override
     public boolean canInteractWith(EntityPlayer par1EntityPlayer)
     {
--- QuantumCraft/research/ResearchHandler.java
+++ QuantumCraft/research/ResearchHandler.java
@@ -10,39 +10,41 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class ResearchHandler {
-	
+
 	ResearchEntityProperties rep;
 	EntityPlayer player;
-	
+
 	public ResearchHandler(EntityPlayer p) {
 		this.player = p;
 		rep = (ResearchEntityProperties) p.getExtendedProperties("QC_Research");
-		if (rep == null)
-		{
-			p.registerExtendedProperties("QC_Research", new ResearchEntityProperties());
-			rep = (ResearchEntityProperties) p.getExtendedProperties("QC_Research");
+		if (rep == null) {
+			p.registerExtendedProperties("QC_Research",
+					new ResearchEntityProperties());
+			rep = (ResearchEntityProperties) p
+					.getExtendedProperties("QC_Research");
 		}
-		
-		ris.add(new ResearchItem("Quantonium Ore", "Mine Quantonium Ore", 0, 0, -2, new ItemStack(Loader.OreQuantonium), null));
-        ris.add(new ResearchItem("Raw Quantonium", "Pick up Raw Quantonium", 1, 0, 0, new ItemStack(Loader.ItemRawQuantonium), ris.get(0)));
-        ris.add(new ResearchItem("Crystallized Quantonium", "Smelt your Raw Quantonium", 2, 2, 0, new ItemStack(Loader.ItemCrystalQuantonium), ris.get(1)));
-        //rep.setUnlocked(0);
+
+		ris.add(new ResearchItem("Quantonium Ore", "Mine Quantonium Ore", 0, 0,
+				-2, new ItemStack(Loader.OreQuantonium), null));
+		ris.add(new ResearchItem("Raw Quantonium", "Pick up Raw Quantonium", 1,
+				0, 0, new ItemStack(Loader.ItemRawQuantonium), ris.get(0)));
+		ris.add(new ResearchItem("Crystallized Quantonium",
+				"Smelt your Raw Quantonium", 2, 2, 0, new ItemStack(
+						Loader.ItemCrystalQuantonium), ris.get(1)));
+		// rep.setUnlocked(0);
 	}
 
 	public LinkedList<ResearchItem> ris = new LinkedList<ResearchItem>();
-		
-	public boolean hasRIUnlocked(ResearchItem ri)
-	{
+
+	public boolean hasRIUnlocked(ResearchItem ri) {
 		return rep.getUnlocked(ri.index);
 	}
-	
-	public boolean canUnlockRI(ResearchItem ri)
-	{
-		if (ri.parentAchievement != null)
-		{
+
+	public boolean canUnlockRI(ResearchItem ri) {
+		if (ri.parentAchievement != null) {
 			return hasRIUnlocked(ri.parentAchievement) ? true : false;
-		} 
+		}
 		return true;
 	}
-	
+
 }
