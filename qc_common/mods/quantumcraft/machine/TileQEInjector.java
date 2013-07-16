2dd5bb27867f9b7a1c7bd781c50ec496dcec1d08
sammko <samko.cavoj@gmail.com>
2013-07-16 10:53:55 +0200

    Get the TileEntity and container done //TODO: GUI and Packets and Loading

---
+++ qc_common/mods/quantumcraft/machine/TileQEInjector.java
@@ -0,0 +1,163 @@
+package mods.quantumcraft.machine;
+
+import mods.quantumcraft.core.interfaces.IQEnergizable;
+import mods.quantumcraft.inventory.SimpleInventory;
+import mods.quantumcraft.net.IQEnergySink;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.network.packet.Packet;
+
+public class TileQEInjector extends TileMachineBase implements
+        ISidedInventory, IQEnergySink {
+
+    public ItemStack[] inventory = new ItemStack[2];
+    private SimpleInventory _inv = new SimpleInventory(2, "qei", 64);
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int var1) {
+        if (var1 == 0) {
+            return new int[]{1};
+        } else return new int[]{0};
+    }
+
+    @Override
+    public boolean canInsertItem(int i, ItemStack itemstack, int j) {
+        return j != 0;
+    }
+
+    @Override
+    public boolean canExtractItem(int i, ItemStack itemstack, int j) {
+        return i != 0;
+    }
+    @Override
+    public int getSizeInventory() {
+        return 2;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int i) {
+        return inventory[i];
+    }
+
+    @Override
+    public ItemStack decrStackSize(int i, int j) {
+        if (this.inventory[i] != null) {
+            ItemStack itemstack;
+
+            if (this.inventory[i].stackSize <= j) {
+                itemstack = this.inventory[i];
+                this.inventory[i] = null;
+                return itemstack;
+            } else {
+                itemstack = this.inventory[i].splitStack(j);
+
+                if (this.inventory[i].stackSize == 0) {
+                    this.inventory[i] = null;
+                }
+
+                return itemstack;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int i) {
+        if (this.inventory[i] != null) {
+            ItemStack itemstack = this.inventory[i];
+            this.inventory[i] = null;
+            return itemstack;
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void setInventorySlotContents(int i, ItemStack itemstack) {
+        this.inventory[i] = itemstack;
+
+        if (itemstack != null
+                && itemstack.stackSize > this.getInventoryStackLimit()) {
+            itemstack.stackSize = this.getInventoryStackLimit();
+        }
+
+        _inv.setInventorySlotContents(i, itemstack);
+
+    }
+
+    public void process()
+    {
+        inventory[1] = inventory[0].copy();
+        decrStackSize(0,1);
+    }
+
+    @Override
+    public String getInvName() {
+        return "Quantum Energy Injector";
+    }
+
+    @Override
+    public boolean isInvNameLocalized() {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+
+        return 1;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer entityplayer) {
+
+        return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord,
+                this.zCoord) == this && entityplayer.getDistanceSq(
+                (double) this.xCoord + 0.5D, (double) this.yCoord + 0.5D,
+                (double) this.zCoord + 0.5D) <= 64.0D;
+    }
+
+    @Override
+    public void openChest() {
+    }
+
+    @Override
+    public void closeChest() {
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+        return true;
+    }
+
+
+    @Override
+    public void onBlockBreak() {
+        _inv.dropContents(worldObj, xCoord, yCoord, zCoord);
+    }
+
+
+    @Override
+    public void updateEntity() {
+        if (inventory[0].getItem() instanceof IQEnergizable) {
+            IQEnergizable e = ((IQEnergizable) inventory[0].getItem());
+            int cycle = 5;
+            if (e.getCurrentQEnergyBuffer() <= (e.getMaxQEnergyValue()-cycle)) {
+                e.setCurrentQEnergyBuffer(e.getCurrentQEnergyBuffer()+cycle);
+            } else {
+                cycle = e.getMaxQEnergyValue() - e.getCurrentQEnergyBuffer();
+                e.setCurrentQEnergyBuffer(e.getCurrentQEnergyBuffer()+cycle);
+            }
+            //this.QEnergyBuffer -= cycle;
+            if (e.getCurrentQEnergyBuffer() == e.getMaxQEnergyValue()) {
+               process();
+            }
+        }
+    }
+
+    @Override
+    public Packet getDescriptionPacket() {
+        return null; //TODO: packeting for QEI
+    }
+}
