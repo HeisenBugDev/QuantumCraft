b56889853a73c93091401f1fec25d0933b8f8293
Theron Boerner <hunterboerner@gmail.com>
2013-08-14 17:13:15 -0500

    finish axe

--- qc_common/mods/quantumcraft/items/tools/ItemQuantumAxe.java
+++ qc_common/mods/quantumcraft/items/tools/ItemQuantumAxe.java
@@ -3,18 +3,21 @@ package mods.quantumcraft.items.tools;
 import mods.quantumcraft.core.Loader;
 import mods.quantumcraft.core.interfaces.IQEnergizable;
 import mods.quantumcraft.util.ItemEnergyUtils;
-import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class ItemQuantumAxe extends ItemAxe implements IQEnergizable {
 
+    int maxQenergyValue = 1000;
+
     public ItemQuantumAxe(int par1) {
         super(par1, Loader.ToolMaterials.QUANTUMTOOL);
-        this.setMaxDamage(32);
+        this.setMaxDamage(maxQenergyValue + 1);
     }
-
-    int maxQenergyValue = 1000;
 
     @Override
     public int getMaxQEnergyValue(ItemStack itemStack) {
@@ -32,8 +35,7 @@ public class ItemQuantumAxe extends ItemAxe implements IQEnergizable {
     }
 
     @Override
-    public boolean getIsRepairable(ItemStack par1ItemStack, ItemStack par2ItemStack)
-    {
+    public boolean getIsRepairable(ItemStack par1ItemStack, ItemStack par2ItemStack) {
         return false;
     }
 
@@ -44,4 +46,36 @@ public class ItemQuantumAxe extends ItemAxe implements IQEnergizable {
         ItemEnergyUtils.setEnergy(itemStack, value);
         return value;
     }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack par1ItemStack, World par2World, int par3, int par4, int par5, int par6,
+                                    EntityLivingBase par7EntityLivingBase) {
+        if ((double) Block.blocksList[par3].getBlockHardness(par2World, par4, par5, par6) != 0.0D) {
+            if (getCurrentQEnergyBuffer(par1ItemStack) > 0) {
+                setCurrentQEnergyBuffer(par1ItemStack, getCurrentQEnergyBuffer(par1ItemStack) - 1);
+                ItemEnergyUtils.setDamage(par1ItemStack, getCurrentQEnergyBuffer(par1ItemStack), maxQenergyValue);
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public float getStrVsBlock(ItemStack par1ItemStack, Block par2Block) {
+        if (getCurrentQEnergyBuffer(par1ItemStack) > 0) {
+            if (!(par2Block.blockMaterial == Material.ground || par2Block.blockMaterial == Material.grass ||
+                    par2Block.blockMaterial == Material.sand)) {
+                return (par2Block.blockMaterial == Material.wood || par2Block.blockMaterial == Material.plants ||
+                        par2Block.blockMaterial == Material.vine) ? this.efficiencyOnProperMaterial :
+                        super.getStrVsBlock(par1ItemStack, par2Block);
+            } else return 0.001F;
+        } else {
+            ItemEnergyUtils.emptyEnergy(par1ItemStack, maxQenergyValue);
+            return 0.01F;
+        }
+
+
+    }
+
+
 }
