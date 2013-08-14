0bf6b378a05856f0d63671545068d367a95ed2cc
Theron Boerner <hunterboerner@gmail.com>
2013-08-14 18:06:46 -0500

    Finish sword fixes #50 fixes #61

--- qc_common/mods/quantumcraft/items/tools/ItemQuantumSword.java
+++ qc_common/mods/quantumcraft/items/tools/ItemQuantumSword.java
@@ -2,17 +2,23 @@ package mods.quantumcraft.items.tools;
 
 import mods.quantumcraft.core.Loader;
 import mods.quantumcraft.core.interfaces.IQEnergizable;
+import mods.quantumcraft.util.ItemEnergyUtils;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.world.World;
 
 public class ItemQuantumSword extends ItemSword implements IQEnergizable {
 
+    int maxQenergyValue = 1000;
+
     public ItemQuantumSword(int id) {
         super(id, Loader.ToolMaterials.QUANTUMTOOL);
-        this.setMaxDamage(maxQenergyValue+1);
+        this.setMaxDamage(maxQenergyValue + 1);
     }
-
-    int maxQenergyValue = 1000;
 
     @Override
     public int getMaxQEnergyValue(ItemStack itemStack) {
@@ -26,13 +32,12 @@ public class ItemQuantumSword extends ItemSword implements IQEnergizable {
 
     @Override
     public int getCurrentQEnergyBuffer(ItemStack itemStack) {
-        return getMaxQEnergyValue(itemStack)-itemStack.getItemDamage();
+        return ItemEnergyUtils.getEnergy(itemStack);
 
     }
 
     @Override
-    public boolean getIsRepairable(ItemStack par1ItemStack, ItemStack par2ItemStack)
-    {
+    public boolean getIsRepairable(ItemStack par1ItemStack, ItemStack par2ItemStack) {
         return false;
     }
 
@@ -40,7 +45,33 @@ public class ItemQuantumSword extends ItemSword implements IQEnergizable {
     public int setCurrentQEnergyBuffer(ItemStack itemStack, int value) {
         if (value < 0) value = 0;
         if (value > getMaxQEnergyValue(itemStack)) value = getMaxQEnergyValue(itemStack);
-        itemStack.setItemDamage(getMaxQEnergyValue(itemStack)-value);
+        ItemEnergyUtils.setEnergy(itemStack, value);
         return value;
     }
+
+    @Override
+    public boolean onBlockDestroyed(ItemStack par1ItemStack, World par2World, int par3, int par4, int par5, int par6,
+                                    EntityLivingBase par7EntityLivingBase) {
+        if ((double) Block.blocksList[par3].getBlockHardness(par2World, par4, par5, par6) != 0.0D) {
+            if (getCurrentQEnergyBuffer(par1ItemStack) > 0) {
+                setCurrentQEnergyBuffer(par1ItemStack, getCurrentQEnergyBuffer(par1ItemStack) - 1);
+                ItemEnergyUtils.setDamage(par1ItemStack, getCurrentQEnergyBuffer(par1ItemStack), maxQenergyValue);
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity) {
+        if (getCurrentQEnergyBuffer(stack) > 0) {
+            setCurrentQEnergyBuffer(stack, getCurrentQEnergyBuffer(stack) - 1);
+            ItemEnergyUtils.setDamage(stack, getCurrentQEnergyBuffer(stack), maxQenergyValue);
+            return false;
+        } else {
+            ItemEnergyUtils.emptyEnergy(stack, maxQenergyValue);
+            return true;
+        }
+    }
+
 }
