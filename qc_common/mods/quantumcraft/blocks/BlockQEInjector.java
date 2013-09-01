bf1b53bf7f2fada3fab554b3f91e6c6d3dcdb83c
Theron Boerner <hunterboerner@gmail.com>
2013-09-01 16:41:21 -0500

    clean up

--- qc_common/mods/quantumcraft/blocks/BlockQEInjector.java
+++ qc_common/mods/quantumcraft/blocks/BlockQEInjector.java
@@ -3,24 +3,13 @@ package mods.quantumcraft.blocks;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import mods.quantumcraft.blocks.abstractblocks.BlockEnergySink;
-import mods.quantumcraft.util.BasicUtils;
-import mods.quantumcraft.core.QuantumCraft;
-import mods.quantumcraft.machine.abstractmachines.TileMachineBase;
 import mods.quantumcraft.machine.TileQEInjector;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.Icon;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.Event;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class BlockQEInjector extends BlockEnergySink {
-
     public BlockQEInjector(int id) {
         super(id, Material.iron);
         setHardness(10F);
