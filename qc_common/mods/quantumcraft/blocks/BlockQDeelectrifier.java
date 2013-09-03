714dfb972fedf943269cbbd894bad50012247b50
Theron Boerner <hunterboerner@gmail.com>
2013-09-02 21:32:56 -0500

    begin work on the de-electrifier. This class does not get loaded yet so it's not going to break anything either way unless there's a compiler error.

---
+++ qc_common/mods/quantumcraft/blocks/BlockQDeelectrifier.java
@@ -0,0 +1,37 @@
+package mods.quantumcraft.blocks;
+
+import mods.quantumcraft.blocks.abstractblocks.BlockEnergySource;
+import mods.quantumcraft.tile.TileQDeelectrifier;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: theron
+ * Date: 9/2/13
+ * Time: 3:09 PM
+ */
+public class BlockQDeelectrifier extends BlockEnergySource{
+
+    public BlockQDeelectrifier(int id, Material material) {
+        super(id, Material.iron);
+    }
+
+    @Override
+    public void registerIcons(IconRegister iconRegister) {
+        iconFront = iconRegister.registerIcon("QuantumCraft:machineQDL_front");
+        iconTop = iconRegister.registerIcon("QuantumCraft:machineQDL_top");
+        iconTopR = iconRegister.registerIcon("QuantumCraft:machineQDL_top_r");
+        iconSide = iconRegister.registerIcon("QuantumCraft:machineQDL_side");
+        iconBottom = iconRegister.registerIcon("QuantumCraft:machineQDL_bottom");
+        iconSide = iconRegister.registerIcon("QuantumCraft:machineQDL_side");
+        iconBack = iconRegister.registerIcon("QuantumCraft:machineQDL_back");
+    }
+
+    @Override
+    public TileEntity createNewTileEntity(World world) {
+        return new TileQDeelectrifier();
+    }
+}
