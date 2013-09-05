8445ea660cb5fbd63d098ea7b64489a5e5e1cf31
sammko <samko.cavoj@gmail.com>
2013-09-05 16:37:37 +0200

    More tooltip stuff

--- qc_common/quantumcraft/gui/GuiQEInjector.java
+++ qc_common/quantumcraft/gui/GuiQEInjector.java
@@ -1,5 +1,8 @@
 package quantumcraft.gui;
 
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import quantumcraft.gui.abstractguis.GuiBase;
 import quantumcraft.gui.handlers.HoverHandler;
 import quantumcraft.inventory.ContainerQEInjector;
@@ -79,84 +82,6 @@ public class GuiQEInjector  extends GuiBase {
         GL11.glPopMatrix();
     }
 
-    protected void drawHoveringText(List par1List, int par2, int par3, FontRenderer font)
-    {
-        if (!par1List.isEmpty())
-        {
-            GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-           // RenderHelper.disableStandardItemLighting();
-            GL11.glDisable(GL11.GL_LIGHTING);
-            GL11.glDisable(GL11.GL_DEPTH_TEST);
-            int k = 0;
-            Iterator iterator = par1List.iterator();
-
-            while (iterator.hasNext())
-            {
-                String s = (String)iterator.next();
-                int l = font.getStringWidth(s);
-
-                if (l > k)
-                {
-                    k = l;
-                }
-            }
-
-            int i1 = par2 + 12;
-            int j1 = par3 - 12;
-            int k1 = 8;
-
-            if (par1List.size() > 1)
-            {
-                k1 += 2 + (par1List.size() - 1) * 10;
-            }
-
-            if (i1 + k > this.width)
-            {
-                i1 -= 28 + k;
-            }
-
-            if (j1 + k1 + 6 > this.height)
-            {
-                j1 = this.height - k1 - 6;
-            }
-
-            this.zLevel = 500F;
-            itemRenderer.zLevel = 500F;
-            int l1 = -267386864;
-            this.drawGradientRect(i1 - 3, j1 - 4, i1 + k + 3, j1 - 3, l1, l1);
-            this.drawGradientRect(i1 - 3, j1 + k1 + 3, i1 + k + 3, j1 + k1 + 4, l1, l1);
-            this.drawGradientRect(i1 - 3, j1 - 3, i1 + k + 3, j1 + k1 + 3, l1, l1);
-            this.drawGradientRect(i1 - 4, j1 - 3, i1 - 3, j1 + k1 + 3, l1, l1);
-            this.drawGradientRect(i1 + k + 3, j1 - 3, i1 + k + 4, j1 + k1 + 3, l1, l1);
-            int i2 = 1347420415;
-            int j2 = (i2 & 16711422) >> 1 | i2 & -16777216;
-            this.drawGradientRect(i1 - 3, j1 - 3 + 1, i1 - 3 + 1, j1 + k1 + 3 - 1, i2, j2);
-            this.drawGradientRect(i1 + k + 2, j1 - 3 + 1, i1 + k + 3, j1 + k1 + 3 - 1, i2, j2);
-            this.drawGradientRect(i1 - 3, j1 - 3, i1 + k + 3, j1 - 3 + 1, i2, i2);
-            this.drawGradientRect(i1 - 3, j1 + k1 + 2, i1 + k + 3, j1 + k1 + 3, j2, j2);
-
-            for (int k2 = 0; k2 < par1List.size(); ++k2)
-            {
-                String s1 = (String)par1List.get(k2);
-                font.drawStringWithShadow(s1, i1, j1, -1);
-
-                if (k2 == 0)
-                {
-                    j1 += 2;
-                }
-
-                j1 += 10;
-            }
-
-            this.zLevel = 0.0F;
-            itemRenderer.zLevel = 0.0F;
-            GL11.glEnable(GL11.GL_LIGHTING);
-            GL11.glEnable(GL11.GL_DEPTH_TEST);
-           // RenderHelper.enableStandardItemLighting();
-            GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-        }
-    }
-
     protected void drawTooltips() {
         if (buffHT[0]) {
             renderTooltipText("Close this GUI", buffHX, buffHY);
@@ -169,7 +94,7 @@ public class GuiQEInjector  extends GuiBase {
     protected void renderTooltipText(String text, int x, int y) {
         List l = new ArrayList();
         l.add(text);
-        this.func_102021_a(l, x, y);
+        this.drawHoveringText(l, x, y, this.fontRenderer);
     }
 
     public int buffHX;
