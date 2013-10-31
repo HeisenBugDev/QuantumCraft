5c4e826eb5579bd2aedc377b986d3df5bfadc036
hunterboerner <hunterboerner@gmail.com>
2013-10-31 12:54:15 -0500

    finish refactoring

--- qc_common/quantumcraft/tile/TileQElectrifier.java
+++ qc_common/quantumcraft/tile/TileQElectrifier.java
@@ -28,7 +28,6 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
     public float currentOutput = 0;
     protected PowerHandler powerHandler;
     private int tickCounter = 0;
-    private int energyBuffer = 0;
     private boolean init = false;
 
     public TileQElectrifier() {
@@ -45,10 +44,6 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
         return 1000;
     }
 
-    @Override
-    public int getCurrentEnergy() {
-        return energyBuffer;
-    }
 
     public boolean isPoweredTile(TileEntity tile, ForgeDirection side) {
         if (tile instanceof IPowerReceptor) {
@@ -81,14 +76,6 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
         }
     }
 
-    @Override
-    public int subtractEnergy(int req) {
-        energyBuffer -= req;
-        if (energyBuffer < 0) energyBuffer = 0;
-        if (energyBuffer > getMaxEnergy()) energyBuffer = getMaxEnergy();
-        return energyBuffer;
-    }
-
     private float getPowerToExtract(TileEntity tile) {
         PowerHandler.PowerReceiver receptor =
                 ((IPowerReceptor) tile).getPowerReceiver(getDirectionFacing().getOpposite());
@@ -107,7 +94,7 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
     }
 
     public float extractEnergy(float min, float max, boolean doExtract) {
-        if (energyBuffer < min)
+        if (getCurrentEnergy() < min)
             return 0;
 
         float actualMax;
@@ -124,15 +111,15 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
         float extracted;
 
 
-        if (energyBuffer >= actualMax) {
+        if (getCurrentEnergy() >= actualMax) {
             extracted = actualMax;
             if (doExtract) {
-                energyBuffer -= actualMax;
+                subtractEnergy((int) actualMax);
             }
         } else {
-            extracted = energyBuffer;
+            extracted = getCurrentEnergy();
             if (doExtract) {
-                energyBuffer = 0;
+                setEnergy(0);
             }
         }
 
@@ -142,7 +129,7 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
     @Override
     public void updateEntity() {
         if (this.getCurrentEnergy() < this.getMaxEnergy()) {
-            //System.out.println("Energy buffer of electrifier: " + energyBuffer);
+            //System.out.println("Energy buffer of electrifier: " + getCurrentEnergy());
             this.addEnergy(this.requestPacket(100));
         }
 
@@ -181,7 +168,7 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
     @Override
     public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
         super.readFromNBT(par1NBTTagCompound);
-        this.energyBuffer = par1NBTTagCompound.getInteger("energyBuffer");
+        this.setEnergy(par1NBTTagCompound.getInteger("energyBuffer"));
         updateNextTick = true;
     }
 
@@ -191,7 +178,7 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
 
     @Override
     public void writeToNBT(NBTTagCompound par1NBTTagCompound) {
-        par1NBTTagCompound.setInteger("energyBuffer", this.energyBuffer);
+        par1NBTTagCompound.setInteger("energyBuffer", this.getCurrentEnergy());
 
         super.writeToNBT(par1NBTTagCompound);
     }
