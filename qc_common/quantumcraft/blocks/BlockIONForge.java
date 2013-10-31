1d9d454cc421afc52d40fd1172c15e5e39d2d8f0
hunterboerner <hunterboerner@gmail.com>
2013-10-31 11:49:54 -0500

    don't need Material #67

--- qc_common/quantumcraft/blocks/BlockIONForge.java
+++ qc_common/quantumcraft/blocks/BlockIONForge.java
@@ -7,7 +7,7 @@ import net.minecraft.world.World;
 import quantumcraft.blocks.abstractblocks.BlockEnergySink;
 
 public class BlockIONForge extends BlockEnergySink {
-    public BlockIONForge(int id, Material material) {
+    public BlockIONForge(int id) {
         super(id, Material.iron);
     }
 
