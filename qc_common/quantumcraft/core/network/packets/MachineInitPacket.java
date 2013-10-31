b7e46b9658a41ad33ce7cee2cb5f6be52d80095d
hunterboerner <hunterboerner@gmail.com>
2013-10-31 13:07:38 -0500

    moving over to new packet

---
+++ qc_common/quantumcraft/core/network/packets/MachineInitPacket.java
@@ -0,0 +1,51 @@
+package quantumcraft.core.network.packets;
+
+import quantumcraft.tile.abstracttiles.TileMachineBase;
+import quantumcraft.util.BasicUtils;
+import quantumcraft.core.network.abstractpackets.CoordinatesPacket;
+import quantumcraft.core.network.abstractpackets.ModernPacket;
+import quantumcraft.tile.TileQElectrifier;
+import quantumcraft.tile.TileQDeelectrifier;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class MachineInitPacket extends CoordinatesPacket {
+
+
+    public NBTTagCompound tiledata;
+
+    public MachineInitPacket(int id) {
+        super(id);
+    }
+
+    @Override
+    public ModernPacket template() {
+        return new MachineInitPacket(getID());
+    }
+
+    @Override
+    public void processPacket(EntityPlayer player) {
+        TileMachineBase tile = getTile(player.worldObj, TileMachineBase.class);
+        if (tile != null) {
+            tile.readFromNBT(tiledata);
+            tile.updateNextTick = true;
+        }
+    }
+
+    @Override
+    public void writeData(DataOutputStream data) throws IOException {
+        super.writeData(data);
+        BasicUtils.writeNBTToData(tiledata, data);
+    }
+
+    @Override
+    public void readData(DataInputStream data) throws IOException {
+        super.readData(data);
+        tiledata = (NBTTagCompound) BasicUtils.readNBTFromData(data);
+
+    }
+}
