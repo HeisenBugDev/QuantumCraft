90085d88290e33d80ee126522475962050eb4893
hunterboerner <hunterboerner@gmail.com>
2013-11-01 10:47:21 -0500

    he wasn't using the giant comment

--- qc_common/quantumcraft/gui/GuiQEInjector.java
+++ qc_common/quantumcraft/gui/GuiQEInjector.java
@@ -12,6 +12,10 @@ import java.util.List;
 
 public class GuiQEInjector extends GuiBase {
 
+    public int buffHX;
+    public int buffHY;
+    public boolean[] buffHT = new boolean[2];
+    public int buffCT = 0;
     private TileQEInjector tile;
     private boolean closeButtonHover = false;
 
@@ -30,7 +34,6 @@ public class GuiQEInjector extends GuiBase {
         addHoverHandler(new HoverHandler(1, this), 206, 50, 12, 68);
         addClickHandler(new ClickHandler(0), 189, 9, 9, 9);
     }
-
 
     protected void drawBackground() {
         if (this.renderI) {
@@ -107,13 +110,6 @@ public class GuiQEInjector extends GuiBase {
 
     }
 
-    public int buffHX;
-    public int buffHY;
-    public boolean[] buffHT = new boolean[2];
-
-    public int buffCT = 0;
-
-
     protected void handleClick() {
         if (buffCT > -1) {
             switch (buffCT) {
@@ -138,42 +134,5 @@ public class GuiQEInjector extends GuiBase {
             handleClick();
         }
     }
-
-
-      /*
-    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
-        String s = this.tile.getInvName();
-        this.fontRenderer.drawString(s, (this.xSize-19) / 2 - this.fontRenderer.getStringWidth(s) / 2, 6, 4210752);
-        //this.fontRenderer.drawString("MAX: " + tile.maxival + "; CUR: " + tile.currentival,
-        //        this.xSize / 2 - this.fontRenderer.getStringWidth(s) / 2, 15, 4210752);
-        this.fontRenderer
-                .drawString(StatCollector.translateToLocal("container.inventory"), 8, this.ySize - 96 + 2, 4210752);
-    }
-
-    @Override
-    protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
-        //GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        //this.mc.renderEngine.func_110577_a("/mods/QuantumCraft/textures/gui/deenergizer.png");
-        this.mc.renderEngine.func_110577_a(new ResourceLocation("quantumcraft", "textures/gui/einjector.png"));
-        int k = (this.width - this.xSize) / 2;
-        int l = (this.height - this.ySize) / 2;
-        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-
-        int h2 = (int) (76 - ((float) tile.getCurrentEnergy() / (float) tile.getMaxEnergy() * 76));
-
-        this.drawTexturedModalRect(k + 177, l + 37 + h2, 195, 16 + h2, 12, 76 - h2);
-        this.drawTexturedModalRect(k + 177, l + 37, 207, 16, 12, 76);
-
-        if (tile.upgradeID != 0) {
-            itemRenderer.renderItemAndEffectIntoGUI(this.fontRenderer, this.mc.renderEngine, new ItemStack(Loader.ItemUpgrade, 1, tile.upgradeID), k+174, l+129);
-        }
-
-        if (tile.maxival != 0) {
-            int h = (int) (16 - ((float) tile.currentival / (float) tile.maxival * 16));
-            this.drawTexturedModalRect(k + 69, l + 34 + h, 195, 0 + h, 16, 16 - h);
-        }
-
-
-    }   */
 
 }
