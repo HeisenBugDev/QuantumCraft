47d9cb90dac326ba1a2eb47f184d87ffe3e44c9d
hunterboerner <hunterboerner@gmail.com>
2013-11-01 10:26:10 -0500

    remove shared methods

--- qc_common/quantumcraft/tile/abstracttiles/TileEnergySource.java
+++ qc_common/quantumcraft/tile/abstracttiles/TileEnergySource.java
@@ -4,66 +4,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import quantumcraft.net.Location;
 
 public abstract class TileEnergySource extends TileMachineBase {
-    private int energyBuffer = 0;
-    /**
-     * @return maximum energy this machine can hold
-     */
-    public abstract int getMaxEnergy();
 
-    /**
-     * @return current energy the machine is holding
-     */
-    public int getCurrentEnergy() {
-        return energyBuffer;
-    }
-
-    /**
-     * Adds energy to current buffer. uses substractEnergy with negative nubers.
-     *
-     * @param req amount to add
-     * @return energy buffer _AFTER_ addition
-     */
-    public int addEnergy(int req) {
-        return subtractEnergy(-req);
-    }
-
-    /**
-     * Subtracts energy from current buffer. INCLUDE ZERO AND MAXCHECKING
-     *
-     * @param req amount to subtract
-     * @return energy buffer _AFTER_ subtraction
-     */
-    public int subtractEnergy(int req) {
-        energyBuffer -= req;
-        if (energyBuffer < 0) energyBuffer = 0;
-        if (energyBuffer > getMaxEnergy()) energyBuffer = getMaxEnergy();
-        return energyBuffer;
-    }
-
-    /**
-     * @param set the value you want to set the energy to
-     */
-    public void setEnergy(int set) {
-        energyBuffer = set;
-    }
-
-    @Override
-    public void readFromNBT(NBTTagCompound par1NBTTagCompound) {
-        super.readFromNBT(par1NBTTagCompound);
-        this.energyBuffer = par1NBTTagCompound.getInteger("energyBuffer");
-        updateNextTick = true;
-    }
-
-    /**
-     * Writes a tile entity to NBT.
-     */
-
-    @Override
-    public void writeToNBT(NBTTagCompound par1NBTTagCompound) {
-        par1NBTTagCompound.setInteger("energyBuffer", this.energyBuffer);
-
-        super.writeToNBT(par1NBTTagCompound);
-    }
 
     public int getQuantumEnergy(Location l, int request) {
         if (getCurrentEnergy() >= request) {
