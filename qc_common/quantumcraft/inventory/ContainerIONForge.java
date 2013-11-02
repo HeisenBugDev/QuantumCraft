61de9570a4217a3fe956879ec96ad89c0affbc89
hunterboerner <hunterboerner@gmail.com>
2013-11-02 16:59:07 -0500

    add some slots

--- qc_common/quantumcraft/inventory/ContainerIONForge.java
+++ qc_common/quantumcraft/inventory/ContainerIONForge.java
@@ -2,21 +2,26 @@ package quantumcraft.inventory;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import quantumcraft.inventory.abstractinv.ContainerBase;
 import quantumcraft.tile.TileIONForge;
 
 public class ContainerIONForge extends ContainerBase {
+
     public TileIONForge tile;
 
     public ContainerIONForge(InventoryPlayer ip, TileIONForge te) {
         super(ip);
+        tile = te;
+        this.addSlotToContainer(new Slot(tile, 0, 20, 34));
+        this.addSlotToContainer(new SlotOutput(tile, 1, 131, 34));
     }
 
     public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
         return null;
     }
-
+    
     @Override
     public boolean canInteractWith(EntityPlayer entityplayer) {
         return this.tile.isUseableByPlayer(entityplayer);
