80838aee938183d0058cde1863d5bff0aed63aa5
sammko <samko.cavoj@gmail.com>
2013-11-02 13:48:49 +0000

    Container refactor

--- qc_common/quantumcraft/inventory/ContainerQDislocator.java
+++ qc_common/quantumcraft/inventory/ContainerQDislocator.java
@@ -6,27 +6,19 @@ import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import quantumcraft.core.Loader;
+import quantumcraft.inventory.abstractinv.ContainerBase;
 import quantumcraft.tile.TileQDislocator;
 
-public class ContainerQDislocator extends Container {
+public class ContainerQDislocator extends ContainerBase {
 
     public TileQDislocator tile;
 
     public ContainerQDislocator(InventoryPlayer ip, TileQDislocator te) {
+        super(ip);
         tile = te;
         this.addSlotToContainer(new SlotCustomInput(tile, 0, 20, 34, Loader.ItemLocationCard));
             this.addSlotToContainer(new SlotCustomInput(tile, 1, 131, 34, Loader.ItemLocationCard));
         int i;
-
-        for (i = 0; i < 3; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                this.addSlotToContainer(new Slot(ip, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
-            }
-        }
-
-        for (i = 0; i < 9; ++i) {
-            this.addSlotToContainer(new Slot(ip, i, 8 + i * 18, 142));
-        }
     }
 
     public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
