9898651fe5910a36c53ef83ab8dafd50cb00494a
hunterboerner <hunterboerner@gmail.com>
2013-11-02 10:38:25 -0500

    more work on that #67

--- qc_common/quantumcraft/tile/TileIONForge.java
+++ qc_common/quantumcraft/tile/TileIONForge.java
@@ -1,8 +1,11 @@
 package quantumcraft.tile;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
 import quantumcraft.tile.abstracttiles.TileEnergySink;
 
-public class TileIONForge extends TileEnergySink {
+public class TileIONForge extends TileEnergySink implements ISidedInventory{
 
     @Override
     public int getMaxEnergy() {
@@ -24,4 +27,81 @@ public class TileIONForge extends TileEnergySink {
 
     }
 
+    @Override
+    public int getSizeInventory() {
+        return 0;
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int i) {
+        return null;
+    }
+
+    @Override
+    public ItemStack decrStackSize(int i, int j) {
+        return null;
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int i) {
+        return null;
+    }
+
+    @Override
+    public void setInventorySlotContents(int i, ItemStack itemstack) {
+
+    }
+
+    @Override
+    public String getInvName() {
+        return null;
+    }
+
+    @Override
+    public boolean isInvNameLocalized() {
+        return false;
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return 0;
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer entityplayer) {
+        return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord,
+                this.zCoord) == this && entityplayer.getDistanceSq(
+                (double) this.xCoord + 0.5D, (double) this.yCoord + 0.5D,
+                (double) this.zCoord + 0.5D) <= 64.0D;
+    }
+
+    @Override
+    public void openChest() {
+
+    }
+
+    @Override
+    public void closeChest() {
+
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int i, ItemStack itemstack) {
+        return false;
+    }
+
+    @Override
+    public int[] getAccessibleSlotsFromSide(int var1) {
+        return new int[0];
+    }
+
+    @Override
+    public boolean canInsertItem(int i, ItemStack itemstack, int j) {
+        return false;
+    }
+
+    @Override
+    public boolean canExtractItem(int i, ItemStack itemstack, int j) {
+        return false;
+    }
 }
