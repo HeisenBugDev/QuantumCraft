be3c4796fc3f01fd248a8b107fe43eef16504d3a
hunterboerner <hunterboerner@gmail.com>
2013-11-02 13:49:14 -0500

    layout GUI for ionforge #67

---
+++ qc_common/quantumcraft/gui/GuiIONForge.java
@@ -0,0 +1,29 @@
+package quantumcraft.gui;
+
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+import quantumcraft.inventory.ContainerIONForge;
+import quantumcraft.tile.TileIONForge;
+
+public class GuiIONForge extends GuiContainer {
+    private TileIONForge tile;
+
+    public GuiIONForge(Container par1Container) {
+        super(par1Container);
+        tile = ((ContainerIONForge) par1Container).tile;
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
+
+    }
+
+    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
+
+    }
+
+    @Override
+    public void initGui() {
+
+    }
+}
