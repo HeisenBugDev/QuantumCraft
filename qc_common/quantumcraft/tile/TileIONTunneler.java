68ceb9fbf6a6bbc2b9d4eac7ffb03dd1ddee6cdf
hunterboerner <hunterboerner@gmail.com>
2013-11-14 12:21:01 -0600

    work on breaking blocks

--- qc_common/quantumcraft/tile/TileIONTunneler.java
+++ qc_common/quantumcraft/tile/TileIONTunneler.java
@@ -1,6 +1,7 @@
 package quantumcraft.tile;
 
 import quantumcraft.tile.abstracttiles.TileEnergySink;
+import quantumcraft.util.BasicUtils;
 
 public class TileIONTunneler extends TileEnergySink {
     @Override
@@ -20,6 +21,17 @@ public class TileIONTunneler extends TileEnergySink {
 
     @Override
     public void updateEntity() {
-        System.out.println("The coords are: " + xCoord + " " + yCoord + " " + zCoord);
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i);
+            int location = xCoord + (i * 3);
+            if (worldObj.getBlockId(location, yCoord, zCoord) != 0) {
+                BasicUtils.getBlockInstance(worldObj, location, yCoord, zCoord)
+                        .dropBlockAsItem(worldObj, location, yCoord,
+                                zCoord, 1, 1);
+            }
+            worldObj.setBlockToAir(location, yCoord, zCoord);
+        }
+
+        //System.out.println("The coords are: " + xCoord + " " + yCoord + " " + zCoord);
     }
 }
