17d44f5e27faf393ac962c64834c1731e7fb866a
hunterboerner <hunterboerner@gmail.com>
2013-11-15 08:59:21 -0600

    delay and stop when done

--- qc_common/quantumcraft/tile/TileIONTunneler.java
+++ qc_common/quantumcraft/tile/TileIONTunneler.java
@@ -5,6 +5,8 @@ import quantumcraft.tile.abstracttiles.TileEnergySink;
 public class TileIONTunneler extends TileEnergySink {
     int length = 0;
     int multiplier = 0;
+    boolean stop = false;
+    int pause = 0;
 
     @Override
     public int getMaxEnergy() {
@@ -23,28 +25,34 @@ public class TileIONTunneler extends TileEnergySink {
 
     @Override
     public void updateEntity() {
-        for (int y = 0; y < 2; y++) {
-            for (int i = 0; i < 3; i++) {
-                switch (i) {
-                    case 0:
-                        multiplier = 0;
-                        break;
-                    case 1:
-                        multiplier = -4;
-                        break;
-                    case 2:
-                        multiplier = 4;
-                        break;
+        if (pause > 5) {
+            if (!stop) {
+                for (int y = 0; y < 2; y++) {
+                    for (int i = 0; i < 3; i++) {
+                        switch (i) {
+                            case 0:
+                                multiplier = 0;
+                                break;
+                            case 1:
+                                multiplier = -4;
+                                break;
+                            case 2:
+                                multiplier = 4;
+                                break;
+                        }
+                        int location = xCoord + multiplier;
+                        worldObj.setBlockToAir(location, yCoord + y, zCoord + length + 1);
+                    }
+
                 }
-                int location = xCoord + multiplier;
-                worldObj.setBlockToAir(location, yCoord + y, zCoord + length + 1);
             }
-
+            if (length >= 10) {
+                length = 0;
+                stop = true;
+            }
+            length++;
+            pause = 0;
         }
-        if (length >= 10) length = 0;
-        length++;
-
-
-        //System.out.println("The coords are: " + xCoord + " " + yCoord + " " + zCoord);
+        pause++;
     }
 }
