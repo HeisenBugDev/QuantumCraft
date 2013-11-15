dcac45b01f6084cba7f1c80b55211e989144cc0e
hunterboerner <hunterboerner@gmail.com>
2013-11-15 08:53:02 -0600

    will now make 3 2x10 pathways 3 blocks apart

--- qc_common/quantumcraft/tile/TileIONTunneler.java
+++ qc_common/quantumcraft/tile/TileIONTunneler.java
@@ -23,24 +23,27 @@ public class TileIONTunneler extends TileEnergySink {
 
     @Override
     public void updateEntity() {
-        for (int i = 0; i < 3; i++) {
-            switch (i) {
-                case 0:
-                    multiplier = 0;
-                    break;
-                case 1:
-                    multiplier = -3;
-                    break;
-                case 2:
-                    multiplier = 3;
-                    break;
+        for (int y = 0; y < 2; y++) {
+            for (int i = 0; i < 3; i++) {
+                switch (i) {
+                    case 0:
+                        multiplier = 0;
+                        break;
+                    case 1:
+                        multiplier = -4;
+                        break;
+                    case 2:
+                        multiplier = 4;
+                        break;
+                }
+                int location = xCoord + multiplier;
+                worldObj.setBlockToAir(location, yCoord + y, zCoord + length + 1);
             }
-            int location = xCoord + (i * multiplier);
-            System.out.println("location: " + location + " multiplier: " + multiplier + " i: " + i);
-            worldObj.setBlockToAir(location, yCoord, zCoord + length + 1);
+
         }
         if (length >= 10) length = 0;
         length++;
+
 
         //System.out.println("The coords are: " + xCoord + " " + yCoord + " " + zCoord);
     }
