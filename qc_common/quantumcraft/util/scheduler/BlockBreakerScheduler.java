858d715aae342d7dd1d806ab0a5b96deb34a2e0f
hunterboerner <hunterboerner@gmail.com>
2013-11-18 17:02:59 -0600

    boolean

--- qc_common/quantumcraft/util/scheduler/BlockBreakerScheduler.java
+++ qc_common/quantumcraft/util/scheduler/BlockBreakerScheduler.java
@@ -21,8 +21,9 @@ public class BlockBreakerScheduler extends Scheduler {
             int x = blocks.get(0).x;
             int y = blocks.get(0).y;
             int z = blocks.get(0).z;
-            breakBlock(x, y, z);
-            blocks.remove(0);
+            if (breakBlock(x, y, z)){
+                blocks.remove(0);
+            }
         }
     }
 
@@ -30,8 +31,9 @@ public class BlockBreakerScheduler extends Scheduler {
         blocks.add(new Coords(x, y, z));
     }
 
-    public void breakBlock(int x, int y, int z) {
+    public boolean breakBlock(int x, int y, int z) {
         tile.worldObj.setBlockToAir(x, y, z);
+        return true;
     }
 
 }
