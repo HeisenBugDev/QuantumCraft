890513c9cce24c6a89ff1465760885f8cd1fc3e6
hunterboerner <hunterboerner@gmail.com>
2013-11-19 12:53:21 -0600

    change request

--- qc_common/quantumcraft/tile/TileQElectrifier.java
+++ qc_common/quantumcraft/tile/TileQElectrifier.java
@@ -118,7 +118,7 @@ public class TileQElectrifier extends TileEnergySink implements IPowerEmitter, I
     @Override
     public void updateEntity() {
         if (this.getCurrentEnergy() < this.getMaxEnergy()) {
-            this.addEnergy(this.requestPacket(100));
+            this.addEnergy(this.requestPacket(10));
         }
 
         if (!init && !isInvalid()) {
