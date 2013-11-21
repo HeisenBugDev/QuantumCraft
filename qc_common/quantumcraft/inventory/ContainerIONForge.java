f2089ba182c7037bf82da8904c2b3623b92419b3
hunterboerner <hunterboerner@gmail.com>
2013-11-20 21:07:34 -0600

    shift clicks! fixes #62

--- qc_common/quantumcraft/inventory/ContainerIONForge.java
+++ qc_common/quantumcraft/inventory/ContainerIONForge.java
@@ -21,7 +21,37 @@ public class ContainerIONForge extends ContainerBase {
     }
 
     public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
-        return null;
+        ItemStack stack = null;
+        Slot slotObject = (Slot) inventorySlots.get(slot);
+        System.out.println(slot);
+        //null checks and checks if the item can be stacked (maxStackSize > 1)
+        if (slotObject != null && slotObject.getHasStack()) {
+            ItemStack stackInSlot = slotObject.getStack();
+            stack = stackInSlot.copy();
+
+            //merges the item into player inventory since its in the tileEntity
+            if (slot > 35) {
+                if (!this.mergeItemStack(stackInSlot, 0, 35, true)) {
+                    return null;
+                }
+            }
+            //places it into the tileEntity is possible since its in the player inventory
+            else if (!this.mergeItemStack(stackInSlot, 36, 37, false) && !this.mergeItemStack(stackInSlot, 38, 39, false)) {
+                return null;
+            }
+
+            if (stackInSlot.stackSize == 0) {
+                slotObject.putStack(null);
+            } else {
+                slotObject.onSlotChanged();
+            }
+
+            if (stackInSlot.stackSize == stack.stackSize) {
+                return null;
+            }
+            slotObject.onPickupFromSlot(player, stackInSlot);
+        }
+        return stack;
     }
 
     @Override
