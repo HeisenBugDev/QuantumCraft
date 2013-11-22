f80aabeb842dee3e91dbaf06cc442fc154d9f359
hunterboerner <hunterboerner@gmail.com>
2013-11-21 19:43:17 -0600

    remove that

--- qc_common/quantumcraft/core/Loader.java
+++ qc_common/quantumcraft/core/Loader.java
@@ -172,8 +172,6 @@ public class Loader {
                 .setUnlocalizedName(Config.NameItemQuantumPlating)
                 .setTextureName(Config.getTextureName(Config.NameItemQuantumPlating));
         LanguageRegistry.addName(ItemQuantumPlating, "Quantum Plating");
-        // [feature] - watson is awesome
-        // [todo] - be even more awesome
     }
 
     public static void initBlocks() {
