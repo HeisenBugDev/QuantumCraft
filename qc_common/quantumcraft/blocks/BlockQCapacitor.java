49ee8e6ba29ee1cbca08ec172e75a0cfe786c8a7
hunterboerner <hunterboerner@gmail.com>
2013-11-22 18:19:27 -0600

    fixes #115

--- qc_common/quantumcraft/blocks/BlockQCapacitor.java
+++ qc_common/quantumcraft/blocks/BlockQCapacitor.java
@@ -6,23 +6,23 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import quantumcraft.blocks.abstractblocks.BlockEnergySink;
+import quantumcraft.net.EnergySourceList;
 import quantumcraft.net.IQEnergySource;
 import quantumcraft.net.Location;
 import quantumcraft.net.QuantumEnergyNet;
 import quantumcraft.tile.TileQCapacitor;
 
-public class BlockQCapacitor extends BlockEnergySink implements IQEnergySource{
+public class BlockQCapacitor extends BlockEnergySink implements IQEnergySource {
 
     public BlockQCapacitor(int id) {
         super(id);
     }
 
     @Override
-    public void onBlockPlacedBy(World world, int x, int y, int z,
-                                EntityLivingBase entity, ItemStack stack) {
-        super.onBlockPlacedBy(world, x, y, z, entity,
-                stack);
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack) {
         QuantumEnergyNet.propagateSourceLocation(world, new Location(x, y, z));
+        super.onBlockPlacedBy(world, x, y, z, entity, stack);
+        QuantumEnergyNet.onAddedLink(world, new Location(x, y, z));
     }
 
     @Override
