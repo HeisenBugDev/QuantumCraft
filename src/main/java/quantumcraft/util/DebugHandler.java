e583f700ce5ce3ac5994cfff55aa5464ceabadf7
hunterboerner <hunterboerner@gmail.com>
2013-11-27 19:56:04 -0600

    add methods

--- src/main/java/quantumcraft/util/DebugHandler.java
+++ src/main/java/quantumcraft/util/DebugHandler.java
@@ -1,11 +1,22 @@
 package quantumcraft.util;
 
 import quantumcraft.core.Config;
+import quantumcraft.tile.abstracttiles.TileMachineBase;
 
 public class DebugHandler {
-    public static void debugPrint(String str){
-        if (Config.debug.getBoolean(false)){
+    public static void debugPrint(String str) {
+        if (Config.debug.getBoolean(false)) {
             System.out.println("[QuantumCraft][Debug] " + str);
         }
+    }
+
+    public static void debugPrint(Object obj) {
+        debugPrint(obj.toString());
+    }
+
+    public static void debugPrint(TileMachineBase tile, String str) {
+        debugPrint(tile.getClass().toString() + " at: [x]" + tile.xCoord + " | [y]" +
+                tile.yCoord + " | [z]" +
+                tile.zCoord + " => " + str);
     }
 }
