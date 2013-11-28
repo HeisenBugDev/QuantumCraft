3a46a2784efea14d5bb6b568e39abe630ce98428
hunterboerner <hunterboerner@gmail.com>
2013-11-28 12:21:19 -0600

    implement that!

--- src/main/java/quantumcraft/tile/TileSteamGenerator.java
+++ src/main/java/quantumcraft/tile/TileSteamGenerator.java
@@ -1,8 +1,13 @@
 package quantumcraft.tile;
 
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidHandler;
 import quantumcraft.tile.abstracttiles.TileMachineBase;
 
-public class TileSteamGenerator extends TileMachineBase{
+public class TileSteamGenerator extends TileMachineBase implements IFluidHandler{
     @Override
     public int getMaxEnergy() {
         return 0;
@@ -17,5 +22,35 @@ public class TileSteamGenerator extends TileMachineBase{
     @Override
     public void onBlockBreak() {
 
+    }
+
+    @Override
+    public int fill(ForgeDirection forgeDirection, FluidStack fluidStack, boolean b) {
+        return 0;
+    }
+
+    @Override
+    public FluidStack drain(ForgeDirection forgeDirection, FluidStack fluidStack, boolean b) {
+        return null;
+    }
+
+    @Override
+    public FluidStack drain(ForgeDirection forgeDirection, int i, boolean b) {
+        return null;
+    }
+
+    @Override
+    public boolean canFill(ForgeDirection forgeDirection, Fluid fluid) {
+        return false;
+    }
+
+    @Override
+    public boolean canDrain(ForgeDirection forgeDirection, Fluid fluid) {
+        return false;
+    }
+
+    @Override
+    public FluidTankInfo[] getTankInfo(ForgeDirection forgeDirection) {
+        return new FluidTankInfo[0];
     }
 }
