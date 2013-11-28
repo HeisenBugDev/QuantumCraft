4bf45c58195929a9d4b9c89cc915648c8657d01e
hunterboerner <hunterboerner@gmail.com>
2013-11-27 19:44:07 -0600

    I'm thinking I could do better

--- src/main/java/quantumcraft/util/DebugHandler.java
+++ src/main/java/quantumcraft/util/DebugHandler.java
@@ -1,4 +1,11 @@
 package quantumcraft.util;
 
+import quantumcraft.core.Config;
+
 public class DebugHandler {
+    public static void debugPrint(String str){
+        if (Config.debug.getBoolean(false)){
+            System.out.println("[QuantumCraft][Debug] " + str);
+        }
+    }
 }
