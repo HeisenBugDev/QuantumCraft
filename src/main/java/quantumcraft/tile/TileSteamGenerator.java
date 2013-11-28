60c14dd4460e2f72fa1c1534f5248d228e57b032
hunterboerner <hunterboerner@gmail.com>
2013-11-28 12:15:58 -0600

    implement base

--- src/main/java/quantumcraft/tile/TileSteamGenerator.java
+++ src/main/java/quantumcraft/tile/TileSteamGenerator.java
@@ -1,4 +1,21 @@
 package quantumcraft.tile;
 
-public class TileSteamGenerator {
+import quantumcraft.tile.abstracttiles.TileMachineBase;
+
+public class TileSteamGenerator extends TileMachineBase{
+    @Override
+    public int getMaxEnergy() {
+        return 0;
+    }
+
+    @Override
+    public int guiID() {
+        // Will be 8 when done
+        return -1;
+    }
+
+    @Override
+    public void onBlockBreak() {
+
+    }
 }
