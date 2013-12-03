51f01b947a554b5ce61f3d8b913df2aa88ce6918
hunterboerner <hunterboerner@gmail.com>
2013-12-03 11:47:19 -0600

    create gui "stub"

---
+++ src/main/java/quantumcraft/gui/GuiSteamGenerator.java
@@ -0,0 +1,25 @@
+package quantumcraft.gui;
+
+import net.minecraft.inventory.Container;
+import quantumcraft.gui.abstractguis.GuiBase;
+import quantumcraft.inventory.ContainerSteamGenerator;
+import quantumcraft.tile.TileSteamGenerator;
+
+public class GuiSteamGenerator extends GuiBase {
+
+    private TileSteamGenerator tile;
+
+    protected GuiSteamGenerator(Container container) {
+        super(container, 200, 170);
+        tile = ((ContainerSteamGenerator) container).tile;
+    }
+
+    @Override protected void handleClick(int buffCT) {
+        if (buffCT > -1) {
+            switch (buffCT) {
+                case 0:
+                    this.mc.thePlayer.closeScreen();
+            }
+        }
+    }
+}
