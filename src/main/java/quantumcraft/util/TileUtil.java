cf90baa8087695b2e31f8c2fe559d47aee16dade
hunterboerner <hunterboerner@gmail.com>
2013-12-04 11:06:12 -0600

    one does not simply NOT write javadocs

--- src/main/java/quantumcraft/util/TileUtil.java
+++ src/main/java/quantumcraft/util/TileUtil.java
@@ -3,7 +3,12 @@ package quantumcraft.util;
 import net.minecraft.item.ItemStack;
 
 public class TileUtil {
-
+    /**
+     * @param i slot
+     * @param j amount to remove
+     * @param inventory inventory array of itemstacks
+     * @return the itemstack that has been modified
+     */
     public static ItemStack decrStackSize(int i, int j, ItemStack[] inventory) {
         if (inventory[i] != null) {
             ItemStack itemstack;
