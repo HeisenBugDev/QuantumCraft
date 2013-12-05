8b55c70af7bdbfed995d3f0bd0e6fd373720e897
hunterboerner <hunterboerner@gmail.com>
2013-12-05 17:33:11 -0600

    closer to better debug

--- src/main/java/quantumcraft/util/DebugHandler.java
+++ src/main/java/quantumcraft/util/DebugHandler.java
@@ -7,14 +7,22 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 public class DebugHandler {
+    private static Date lastDate = new Date();
+
     public static void debugPrint(String str) {
         if (Config.debug != null) {
             if (Config.debug.getBoolean(false)) {
                 try {
                     PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("QuantumCraft.log", true)));
-                    out.println("[QuantumCraft][Debug] " + str);
+                    String time = "";
+                    if (new Date().getTime() != lastDate.getTime()){
+                        lastDate = new Date();
+                        time = lastDate + "\n";
+                    }
+                    out.println(time + "\t[QuantumCraft][Debug] " + str);
                     out.close();
                 } catch (IOException e) {
                     //oh noes!
