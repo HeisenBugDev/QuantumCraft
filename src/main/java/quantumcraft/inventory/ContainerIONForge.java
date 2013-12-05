aa567e26d806001d74b473eb9862a5b468285fa8
hunterboerner <hunterboerner@gmail.com>
2013-12-04 18:36:03 -0600

    fixes #135

--- src/main/java/quantumcraft/inventory/ContainerIONForge.java
+++ src/main/java/quantumcraft/inventory/ContainerIONForge.java
@@ -15,43 +15,11 @@ public class ContainerIONForge extends ContainerUpdatedBase {
     public ContainerIONForge(InventoryPlayer ip, TileIONForge te) {
         super(ip);
         tile = te;
+        slots.add(42);
         this.addSlotToContainer(new Slot(tile, 0, 40, 48));
         this.addSlotToContainer(new SlotOutput(tile, 1, 75, 48));
         this.addSlotToContainer(new Slot(tile, 2, 40, 76));
         this.addSlotToContainer(new SlotOutput(tile, 3, 75, 76));
-    }
-
-    public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
-        ItemStack stack = null;
-        Slot slotObject = (Slot) inventorySlots.get(slot);
-        //null checks and checks if the item can be stacked (maxStackSize > 1)
-        if (slotObject != null && slotObject.getHasStack()) {
-            ItemStack stackInSlot = slotObject.getStack();
-            stack = stackInSlot.copy();
-
-            //merges the item into player inventory since its in the tileEntity
-            if (slot > 39) {
-                if (!this.mergeItemStack(stackInSlot, 0, 35, true)) {
-                    return null;
-                }
-            }
-            //places it into the tileEntity is possible since its in the player inventory
-            else if (!this.mergeItemStack(stackInSlot, 40, 41, false) && !this.mergeItemStack(stackInSlot, 42, 43, false)) {
-                return null;
-            }
-
-            if (stackInSlot.stackSize == 0) {
-                slotObject.putStack(null);
-            } else {
-                slotObject.onSlotChanged();
-            }
-
-            if (stackInSlot.stackSize == stack.stackSize) {
-                return null;
-            }
-            slotObject.onPickupFromSlot(player, stackInSlot);
-        }
-        return stack;
     }
 
     @Override
--- src/main/java/quantumcraft/inventory/ContainerQDematerializer.java
+++ src/main/java/quantumcraft/inventory/ContainerQDematerializer.java
@@ -18,40 +18,6 @@ public class ContainerQDematerializer extends ContainerUpdatedBase {
         this.addSlotToContainer(new Slot(tile, 0, 40, 60));
     }
 
-    public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
-        ItemStack stack = null;
-        Slot slotObject = (Slot) inventorySlots.get(slot);
-
-        //null checks and checks if the item can be stacked (maxStackSize > 1)
-        if (slotObject != null && slotObject.getHasStack()) {
-            ItemStack stackInSlot = slotObject.getStack();
-            stack = stackInSlot.copy();
-
-            //merges the item into player inventory since its in the tileEntity
-            if (slot > 39) {
-                if (!this.mergeItemStack(stackInSlot, 0, 35, true)) {
-                    return null;
-                }
-            }
-            //places it into the tileEntity is possible since its in the player inventory
-            else if (!this.mergeItemStack(stackInSlot, 40, 41, false)) {
-                return null;
-            }
-
-            if (stackInSlot.stackSize == 0) {
-                slotObject.putStack(null);
-            } else {
-                slotObject.onSlotChanged();
-            }
-
-            if (stackInSlot.stackSize == stack.stackSize) {
-                return null;
-            }
-            slotObject.onPickupFromSlot(player, stackInSlot);
-        }
-        return stack;
-    }
-
     @Override
     public boolean canInteractWith(EntityPlayer par1EntityPlayer) {
         return this.tile.isUseableByPlayer(par1EntityPlayer);
