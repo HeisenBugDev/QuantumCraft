5fed78b10e136f1aed83560ac39f6eb9a7ce230d
hunterboerner <hunterboerner@gmail.com>
2013-12-05 17:24:52 -0600

    print to file

--- src/main/java/quantumcraft/util/DebugHandler.java
+++ src/main/java/quantumcraft/util/DebugHandler.java
@@ -3,11 +3,22 @@ package quantumcraft.util;
 import quantumcraft.core.Config;
 import quantumcraft.tile.abstracttiles.TileMachineBase;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class DebugHandler {
     public static void debugPrint(String str) {
         if (Config.debug != null) {
             if (Config.debug.getBoolean(false)) {
-                System.out.println("[QuantumCraft][Debug] " + str);
+                try {
+                    PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("QuantumCraft.log", true)));
+                    out.println("[QuantumCraft][Debug] " + str);
+                    out.close();
+                } catch (IOException e) {
+                    //oh noes!
+                }
             }
         }
     }
