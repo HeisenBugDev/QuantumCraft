50d2e7063c1055d6f511899899e97a3c7afb1744
hunterboerner <hunterboerner@gmail.com>
2013-12-07 15:18:07 -0600

    better message

--- src/main/java/quantumcraft/util/DebugPrompt.java
+++ src/main/java/quantumcraft/util/DebugPrompt.java
@@ -3,14 +3,16 @@ package quantumcraft.util;
 import javax.swing.*;
 import java.awt.*;
 
-public class DebugPrompt extends JFrame{
-    public DebugPrompt(String hastebinCode){
+public class DebugPrompt extends JFrame {
+    public DebugPrompt(String hastebinCode) {
         setTitle("QuantumCraft Debug");
         // Use the dafault metal styled titlebar
         getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
         // Set the style of the frame
 
-        add(new JTextArea("http://hastebin.com/" + hastebinCode, 5, 25));
+        add(new JTextArea("These are the logs we have collected, please give this link to the person " +
+                "helping you solve the problem: http://hastebin.com/" +
+                hastebinCode, 5, 25));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
         setLayout(new FlowLayout());
