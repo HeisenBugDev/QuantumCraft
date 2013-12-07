040dc3f623d6a6520758a4f44b998810737124ec
hunterboerner <hunterboerner@gmail.com>
2013-12-07 15:23:26 -0600

    add debug prompt

--- src/main/java/quantumcraft/core/Config.java
+++ src/main/java/quantumcraft/core/Config.java
@@ -181,6 +181,11 @@ public class Config {
                 "not a \"feature\". We are not Microcrap™ so be warned. Also, by doing\n" +
                 "this you are either making a really good or really bad choice. I hope\n" +
                 "you know which one it is because I don't");
+
+        debugPrompt = config.get("general", "Be prompted with debug log", true);
+        debugPrompt.comment = ("If you have debug enabled and would like to be given a link to the uploaded logs " +
+                "AUTOMATICALLY whenever you close the game. This should only be disabled by devs because otherwise it" +
+                " is mostly useless having debug enabled");
         config.save();
     }
 
