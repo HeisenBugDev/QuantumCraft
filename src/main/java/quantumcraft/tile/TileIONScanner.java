5d35aedcab2c396b02b77420a36698e703e153d0
hunterboerner <hunterboerner@gmail.com>
2013-12-21 12:55:51 -0600

    mostly fix #148

--- src/main/java/quantumcraft/tile/TileIONScanner.java
+++ src/main/java/quantumcraft/tile/TileIONScanner.java
@@ -11,7 +11,7 @@ import quantumcraft.util.scheduler.IONScannerScheduler;
 public class TileIONScanner extends TileEnergySink {
 
     private IONScannerScheduler scheduler = new IONScannerScheduler(100, this);
-    private int iteration = 0;
+    private int xLoop = 0;
 
     @Override
     public int getMaxEnergy() {
@@ -30,26 +30,26 @@ public class TileIONScanner extends TileEnergySink {
 
     private void scan() {
         scheduler.resetHarvesters();
-        for (int x = -30; x <= 30; x++) {
-            for (int z = -30; z <= 30; z++) {
-                for (int y = -30; y <= 30; y++) {
-                    Block thisBlock = BasicUtils.getBlockInstance(worldObj, xCoord + x, yCoord + y, zCoord + z);
-                    if (thisBlock != null) {
-                        String name = OreDictionary.getOreName(OreDictionary.getOreID(new ItemStack(thisBlock)));
-                        if (name.contains("ore")) {
-                            scheduler.add(xCoord + x, yCoord + y, zCoord + z);
+        if (xLoop >= 30) {
+            xLoop = 0;
+        } else xLoop++;
+        for (int z = -30; z <= 30; z++) {
+            for (int y = -30; y <= 30; y++) {
+                Block thisBlock = BasicUtils.getBlockInstance(worldObj, xCoord + xLoop, yCoord + y, zCoord + z);
+                if (thisBlock != null) {
+                    String name = OreDictionary.getOreName(OreDictionary.getOreID(new ItemStack(thisBlock)));
+                    if (name.contains("ore")) {
+                        scheduler.add(xCoord + xLoop, yCoord + y, zCoord + z);
 
-                        }
-                        TileEntity te = worldObj.getBlockTileEntity(xCoord + x, yCoord + y, zCoord + z);
+                    }
+                    TileEntity te = worldObj.getBlockTileEntity(xCoord + xLoop, yCoord + y, zCoord + z);
 
-                        if (te != null) {
-                            if (te instanceof TileIONHarvester) {
-                                scheduler.addHarvester((TileIONHarvester) te);
-                            }
+                    if (te != null) {
+                        if (te instanceof TileIONHarvester) {
+                            scheduler.addHarvester((TileIONHarvester) te);
                         }
                     }
                 }
-
             }
         }
     }
@@ -63,10 +63,7 @@ public class TileIONScanner extends TileEnergySink {
         if (this.getCurrentEnergy() > 0) {
             // Requires a constant feed of power.
             subtractEnergy(1);
-            if (iteration < 100) {
-                iteration++;
-            } else {
-                iteration = 0;
+            if (worldObj.getWorldTime() % 20 == 0) {
                 scan();
             }
             scheduler.checkHarvesters(worldObj);
