bbde2344ff6a0a32e7ac76ae4d938daf490828cb
hunterboerner <hunterboerner@gmail.com>
2013-12-25 14:54:04 -0600

    make removal blocks look pretty. We may wanna put a texture on it but it looks ok. Only problem I see is that if you are not touching any blocks with your cursor (so that the block box shows up) you are unable to see it. Will debug later... I have something better to do (secret time) #155

--- src/main/java/quantumcraft/render/BlockHighlighterHandler.java
+++ src/main/java/quantumcraft/render/BlockHighlighterHandler.java
@@ -2,6 +2,7 @@ package quantumcraft.render;
 
 import cpw.mods.fml.client.FMLClientHandler;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.common.ForgeDirection;
@@ -9,28 +10,36 @@ import net.minecraftforge.event.ForgeSubscribe;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 import quantumcraft.core.QuantumCraft;
+import quantumcraft.util.BasicUtils;
+import quantumcraft.util.Coords;
+import quantumcraft.util.IONTunnelerGlobalRemovalList;
+
+import java.util.ArrayList;
 
 public class BlockHighlighterHandler {
 
     @ForgeSubscribe
-    public void onDrawBlockHighlightEvent(DrawBlockHighlightEvent event){
-        if ((event.target.blockX == 0) && (event.target.blockY == 50) && (event.target.blockZ == 0)){
-            drawInWorldTransmutationOverlay(event);
+    public void onDrawBlockHighlightEvent(DrawBlockHighlightEvent event) {
+        if (IONTunnelerGlobalRemovalList.blocks == null) return;
+        for (int i = 0; i < IONTunnelerGlobalRemovalList.blocks.size(); i++) {
+            ArrayList<Coords> blocks = IONTunnelerGlobalRemovalList.getBlocks();
+            if(event.player.getEntityWorld().getBlockId(blocks.get(i).x, blocks.get(i).y, blocks.get(i).z) != 0)
+            drawInWorldTransmutationOverlay(event, IONTunnelerGlobalRemovalList.blocks.get(i));
         }
     }
-    public static void renderPulsingQuad(ResourceLocation texture, float maxTransparency)
-    {
 
-        FMLClientHandler.instance().getClient().renderEngine.bindTexture(texture);
+    public static void renderPulsingQuad(ResourceLocation texture, float maxTransparency) {
+
+        FMLClientHandler.instance().getClient().renderEngine.bindTexture(new ResourceLocation(""));
         Tessellator tessellator = Tessellator.instance;
 
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-        GL11.glColor3f(1, 1, 1);
+        GL11.glColor3f(1, 1, 0);
 
         tessellator.startDrawingQuads();
-        tessellator.setColorRGBA_F(1, 1, 1, 0.5F);
+        tessellator.setColorRGBA_F(1, 0, 0, 0.5F);
 
         tessellator.addVertexWithUV(-0.5D, 0.5D, 0F, 0, 1);
         tessellator.addVertexWithUV(0.5D, 0.5D, 0F, 1, 1);
@@ -41,12 +50,12 @@ public class BlockHighlighterHandler {
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
     }
-    public void drawInWorldTransmutationOverlay(DrawBlockHighlightEvent event)
-    {
 
-        double x = event.target.blockX;
-        double y = event.target.blockY;
-        double z = event.target.blockZ;
+    public void drawInWorldTransmutationOverlay(DrawBlockHighlightEvent event, Coords coord) {
+
+        double x = coord.x;
+        double y = coord.y;
+        double z = coord.z;
         double iPX = event.player.prevPosX + (event.player.posX - event.player.prevPosX) * event.partialTicks;
         double iPY = event.player.prevPosY + (event.player.posY - event.player.prevPosY) * event.partialTicks;
         double iPZ = event.player.prevPosZ + (event.player.posZ - event.player.prevPosZ) * event.partialTicks;
@@ -60,8 +69,7 @@ public class BlockHighlighterHandler {
         GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_CULL_FACE);
 
-        for (int i = 0; i < 6; i++)
-        {
+        for (int i = 0; i < 6; i++) {
             ForgeDirection forgeDir = ForgeDirection.getOrientation(i);
             int zCorrection = i == 2 ? -1 : 1;
             GL11.glPushMatrix();
