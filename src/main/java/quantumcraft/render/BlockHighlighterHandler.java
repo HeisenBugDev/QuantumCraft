05688079c1ebe5a2356848a53b5e5b1491820548
hunterboerner <hunterboerner@gmail.com>
2013-12-25 13:29:11 -0600

    draw the texture slightly outside of the block, this is an early concept.

--- src/main/java/quantumcraft/render/BlockHighlighterHandler.java
+++ src/main/java/quantumcraft/render/BlockHighlighterHandler.java
@@ -1,98 +1,62 @@
 package quantumcraft.render;
 
+import cpw.mods.fml.client.FMLClientHandler;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.common.ForgeDirection;
 import net.minecraftforge.event.ForgeSubscribe;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL12;
+import quantumcraft.core.QuantumCraft;
 
 public class BlockHighlighterHandler {
+
     @ForgeSubscribe
     public void onDrawBlockHighlightEvent(DrawBlockHighlightEvent event){
         if ((event.target.blockX == 0) && (event.target.blockY == 50) && (event.target.blockZ == 0)){
             drawInWorldTransmutationOverlay(event);
         }
     }
+    public static void renderPulsingQuad(ResourceLocation texture, float maxTransparency)
+    {
 
+        FMLClientHandler.instance().getClient().renderEngine.bindTexture(texture);
+        Tessellator tessellator = Tessellator.instance;
+
+        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glColor3f(1, 1, 1);
+
+        tessellator.startDrawingQuads();
+        tessellator.setColorRGBA_F(1, 1, 1, 0.5F);
+
+        tessellator.addVertexWithUV(-0.5D, 0.5D, 0F, 0, 1);
+        tessellator.addVertexWithUV(0.5D, 0.5D, 0F, 1, 1);
+        tessellator.addVertexWithUV(0.5D, -0.5D, 0F, 1, 0);
+        tessellator.addVertexWithUV(-0.5D, -0.5D, 0F, 0, 0);
+
+        tessellator.draw();
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+    }
     public void drawInWorldTransmutationOverlay(DrawBlockHighlightEvent event)
     {
 
-        double x = event.target.blockX + 0.5F;
-        double y = event.target.blockY + 0.5F;
-        double z = event.target.blockZ + 0.5F;
+        double x = event.target.blockX;
+        double y = event.target.blockY;
+        double z = event.target.blockZ;
         double iPX = event.player.prevPosX + (event.player.posX - event.player.prevPosX) * event.partialTicks;
         double iPY = event.player.prevPosY + (event.player.posY - event.player.prevPosY) * event.partialTicks;
         double iPZ = event.player.prevPosZ + (event.player.posZ - event.player.prevPosZ) * event.partialTicks;
 
-        float xScale = 1;
-        float yScale = 1;
-        float zScale = 1;
-        float xShift = 0.1F;
-        float yShift = 0.1F;
-        float zShift = 0.1F;
-        int chargeLevel;
-        int itemChargeLevel = 0;
-
-        chargeLevel = 1 + itemChargeLevel * 2;
-
-        ForgeDirection sideHit = ForgeDirection.getOrientation(event.target.sideHit);
-
-        switch (sideHit)
-        {
-            case UP:
-            {
-                xScale = chargeLevel + 0.1F;
-                zScale = chargeLevel + 0.1F;
-                xShift = 0;
-                zShift = 0;
-                break;
-            }
-            case DOWN:
-            {
-                xScale = chargeLevel + 0.1F;
-                zScale = chargeLevel + 0.1F;
-                xShift = 0;
-                yShift = -yShift;
-                zShift = 0;
-                break;
-            }
-            case NORTH:
-            {
-                xScale = chargeLevel + 0.1F;
-                yScale = chargeLevel + 0.1F;
-                xShift = 0;
-                yShift = 0;
-                zShift = -zShift;
-                break;
-            }
-            case SOUTH:
-            {
-                xScale = chargeLevel + 0.1F;
-                yScale = chargeLevel + 0.1F;
-                xShift = 0;
-                yShift = 0;
-                break;
-            }
-            case EAST:
-            {
-                yScale = chargeLevel + 0.1F;
-                zScale = chargeLevel + 0.1F;
-                yShift = 0;
-                zShift = 0;
-                break;
-            }
-            case WEST:
-            {
-                yScale = chargeLevel + 0.1F;
-                zScale = chargeLevel + 0.1F;
-                xShift = -xShift;
-                yShift = 0;
-                zShift = 0;
-                break;
-            }
-            default:
-                break;
-        }
-
+        float xScale = 1.1F;
+        float yScale = 1.1F;
+        float zScale = 1.1F;
+        float xShift = 0.5F;
+        float yShift = 0.5F;
+        float zShift = 0.5F;
         GL11.glDepthMask(false);
         GL11.glDisable(GL11.GL_CULL_FACE);
 
@@ -101,12 +65,13 @@ public class BlockHighlighterHandler {
             ForgeDirection forgeDir = ForgeDirection.getOrientation(i);
             int zCorrection = i == 2 ? -1 : 1;
             GL11.glPushMatrix();
-            GL11.glColor3f(1,0,0);
+            GL11.glColor3f(1, 0, 0);
             GL11.glTranslated(-iPX + x + xShift, -iPY + y + yShift, -iPZ + z + zShift);
             GL11.glScalef(1F * xScale, 1F * yScale, 1F * zScale);
             GL11.glRotatef(90, forgeDir.offsetX, forgeDir.offsetY, forgeDir.offsetZ);
             GL11.glTranslated(0, 0, 0.5f * zCorrection);
             GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
+            renderPulsingQuad(QuantumCraft.overlayTex, 0.75F);
             GL11.glPopMatrix();
         }
 
