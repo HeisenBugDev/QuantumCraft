7e7fd69f131a5f15f54996d7b93859434057a3e5
hunterboerner <hunterboerner@gmail.com>
2013-12-27 09:33:12 -0600

    Create block class for QIG

---
+++ src/main/java/quantumcraft/blocks/BlockQInterdimensionalGenerator.java
@@ -0,0 +1,20 @@
+package quantumcraft.blocks;
+
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import quantumcraft.blocks.abstractblocks.BlockEnergySink;
+
+public class BlockQInterdimensionalGenerator extends BlockEnergySink{
+    public BlockQInterdimensionalGenerator(int id) {
+        super(id);
+    }
+
+    @Override public void registerIcons(IconRegister iconRegister) {
+
+    }
+
+    @Override public TileEntity createNewTileEntity(World world) {
+        return null;
+    }
+}
