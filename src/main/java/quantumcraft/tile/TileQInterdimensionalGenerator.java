51d7e6563aafe3b55478cba83d50c4df06ac4eaa
hunterboerner <hunterboerner@gmail.com>
2013-12-27 13:56:20 -0600

    TileQIG logic done. Such simple. Much easy.

--- src/main/java/quantumcraft/tile/TileQInterdimensionalGenerator.java
+++ src/main/java/quantumcraft/tile/TileQInterdimensionalGenerator.java
@@ -2,11 +2,15 @@ package quantumcraft.tile;
 
 import net.minecraft.nbt.NBTTagCompound;
 import quantumcraft.tile.abstracttiles.TileEnergySource;
+import quantumcraft.util.BasicUtils;
 import quantumcraft.util.Coords;
 import quantumcraft.util.QInterdimensionalGeneratorDataObject;
 import quantumcraft.util.QInterdimensionalGeneratorUtil;
 
 public class TileQInterdimensionalGenerator extends TileEnergySource {
+
+    private int generatorsInChunk = 1;
+
     @Override
     public int getMaxEnergy() {
         return 300;
@@ -23,8 +27,23 @@ public class TileQInterdimensionalGenerator extends TileEnergySource {
 
     }
 
-    public void onQIGChange() {
+    @Override
+    public void updateEntity() {
+        super.updateEntity();
+        if (worldObj.getWorldTime() % 20 == 0) this.addEnergy(10 / generatorsInChunk);
+    }
 
+    public void onQIGChange() {
+        int xChunk = BasicUtils.getChunk(xCoord);
+        int zChunk = BasicUtils.getChunk(zCoord);
+
+        generatorsInChunk = 0;
+
+        for (QInterdimensionalGeneratorDataObject generator : QInterdimensionalGeneratorUtil.getGenerators()) {
+            int localXChunk = BasicUtils.getChunk(generator.getCoords().x);
+            int localZChunk = BasicUtils.getChunk(generator.getCoords().z);
+            if (xChunk == localXChunk && zChunk == localZChunk && generator.getWorld() == worldObj) generatorsInChunk++;
+        }
     }
 
     @Override
