dbb9a024fe3b35e63dff998d6953d9e5e01668a3
hunterboerner <hunterboerner@gmail.com>
2013-12-27 10:09:39 -0600

    (BlockQIG) Block place and break method calls.

--- src/main/java/quantumcraft/blocks/BlockQInterdimensionalGenerator.java
+++ src/main/java/quantumcraft/blocks/BlockQInterdimensionalGenerator.java
@@ -1,10 +1,14 @@
 package quantumcraft.blocks;
 
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import quantumcraft.blocks.abstractblocks.BlockEnergySource;
 import quantumcraft.tile.TileQInterdimensionalGenerator;
+import quantumcraft.util.Coords;
+import quantumcraft.util.QIGUtil;
 
 public class BlockQInterdimensionalGenerator extends BlockEnergySource {
     public BlockQInterdimensionalGenerator(int id) {
@@ -19,5 +23,18 @@ public class BlockQInterdimensionalGenerator extends BlockEnergySource {
     @Override
     public TileEntity createNewTileEntity(World world) {
         return new TileQInterdimensionalGenerator();
+    }
+
+    @Override
+    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack) {
+        super.onBlockPlacedBy(world, x, y, z, entity, stack);
+        QIGUtil.addGenerator(new Coords(x, y, z));
+        QIGUtil.updateAllGenerators(world);
+    }
+
+    @Override
+    public void breakBlock(World world, int x, int y, int z, int par5, int par6) {
+        super.breakBlock(world, x, y, z, par5, par6);
+        QIGUtil.removeGeneratorFromNewCoords(new Coords(x, y, z));
     }
 }
