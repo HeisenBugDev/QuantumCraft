3c95c6fc55c8419f6f3db303d6ad8116c8983b91
hunterboerner <hunterboerner@gmail.com>
2013-12-27 09:44:01 -0600

    Y U overrides on same line!

--- src/main/java/quantumcraft/blocks/BlockQInterdimensionalGenerator.java
+++ src/main/java/quantumcraft/blocks/BlockQInterdimensionalGenerator.java
@@ -5,16 +5,18 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import quantumcraft.blocks.abstractblocks.BlockEnergySink;
 
-public class BlockQInterdimensionalGenerator extends BlockEnergySink{
+public class BlockQInterdimensionalGenerator extends BlockEnergySink {
     public BlockQInterdimensionalGenerator(int id) {
         super(id);
     }
 
-    @Override public void registerIcons(IconRegister iconRegister) {
+    @Override
+    public void registerIcons(IconRegister iconRegister) {
 
     }
 
-    @Override public TileEntity createNewTileEntity(World world) {
+    @Override
+    public TileEntity createNewTileEntity(World world) {
         return null;
     }
 }
