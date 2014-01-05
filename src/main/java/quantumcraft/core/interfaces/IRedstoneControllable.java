1d7fc50fe1d8b2dc91cc7c2eb67eea2574910650
sammko <samko.cavoj@gmail.com>
2014-01-05 15:28:41 +0100

    rever ignore that too (reverted from commit cba1fd64aeedd5f78ea4c8bc144b2270a6cba13c)

--- src/main/java/quantumcraft/core/interfaces/IRedstoneControllable.java
+++
@@ -1,7 +0,0 @@
-package quantumcraft.core.interfaces;
-
-/**
- * Created by sammko on 1/4/14.
- */
-public interface IRedstoneControllable {
-}
--- src/main/java/quantumcraft/core/interfaces/RedstoneControl.java
+++
@@ -1,7 +0,0 @@
-package quantumcraft.core.interfaces;
-
-public enum RedstoneControl {
-    IGNORE = 0,
-    RUN_ON_PWR = 1,
-    STOP_ON_PWR = 2
-}
