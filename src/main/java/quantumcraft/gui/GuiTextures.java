12b30be0248b34a2652afb832e0bb8d03d742342
hunterboerner <hunterboerner@gmail.com>
2014-03-04 13:21:40 -0600

    Renaming

--- src/main/java/quantumcraft/gui/GuiTextures.java
+++ src/main/java/quantumcraft/gui/GuiTextures.java
@@ -5,37 +5,37 @@ import net.minecraft.util.ResourceLocation;
 public class GuiTextures {
 
     public static ResourceLocation GUI_BASE =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/base.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/base.png");
     public static ResourceLocation GUI_RGYB =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/RGYB_overlay.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/RGYB_overlay.png");
     public static ResourceLocation GUI_WHITELINE =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/whiteline2.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/whiteline2.png");
     public static ResourceLocation GUI_COLOR_STRIP =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/col_strip.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/col_strip.png");
     public static ResourceLocation GUI_TOP_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/top_background.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/top_background.png");
     public static ResourceLocation GUI_BOTTOM_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/bottom_background.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/bottom_background.png");
     public static ResourceLocation GUI_INVENTORY_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/inv_background.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/inv_background.png");
     public static ResourceLocation GUI_ARMOR_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/armor_background.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/armor_background.png");
     public static ResourceLocation GUI_POWER_BAR =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/powerbar.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/powerbar.png");
     public static ResourceLocation GUI_BUTTON_CLOSE =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/button_close.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/button_close.png");
     public static ResourceLocation GUI_DIVIDER_V =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/divider_vertical.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/divider_vertical.png");
     public static ResourceLocation GUI_1SLOT_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/1_slot.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/1_slot.png");
     public static ResourceLocation GUI_2SLOT_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/2_slot.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/2_slot.png");
     public static ResourceLocation GUI_4SLOT_BG =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/4_slot.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/4_slot.png");
     public static ResourceLocation GUI_PROGRESS_BELOW =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/progress_below.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/progress_below.png");
     public static ResourceLocation GUI_DEMAT_ARROW =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/dematerializer.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/dematerializer.png");
     public static ResourceLocation GUI_CAPACITOR_2SLOT =
-            new ResourceLocation("quantumcraft", "assets.quantumcraft.textures/gui/capacitor_2slot.png");
+            new ResourceLocation("QuantumCraft", "assets.quantumcraft.textures/gui/capacitor_2slot.png");
 }
